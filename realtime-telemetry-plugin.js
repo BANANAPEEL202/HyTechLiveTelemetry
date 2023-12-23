9a79fa9005936aa1a31d229e18fc009386e50704
BANANAPEEL202 <bbob52645@gmail.com>
2023-12-23 14:07:49 -0800

    changed to use test.mosquitto.org

--- realtime-telemetry-plugin.js
+++ realtime-telemetry-plugin.js
@@ -4,7 +4,7 @@
 import Paho from 'paho-mqtt'; 
 export function RealtimeTelemetryPlugin() {
     return function (openmct) {
-        var client = new Paho.Client('localhost', 80, "/", "unique_ID");
+        var client = new Paho.Client('test.mosquitto.org', 1883, "/", "unique_ID");
         var globalChannels = [];
         const myClientConnected = function () {
             console.log("connected");
